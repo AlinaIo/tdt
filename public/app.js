9aea5483afb000660e92b411e42affa62f5506ce
Alina <alinaionescu85@gmail.com>
2016-09-19 18:38:25 +0300

    added css and js write to app.js

--- public/app.js
+++ public/app.js
@@ -30,15 +30,33 @@ var server = http.createServer(function (req, res) {
 
 });
 
-function displayForm(res) {
+function displayForm(res, req) {
+
+
     fs.readFile('public/login.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-            'Content-Length': data.length
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': data.length
+            });
+            res.write(data);
+            // res.end();
         });
-        res.write(data);
-        res.end();
-    });
+
+    fs.readFile(__dirname + '/login.js', function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.write(data);
+         // res.end();
+        });
+
+    fs.readFile(__dirname + '/login.css', function (err, data) {
+            if (err) console.log(err);
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(data);
+            res.end();
+        });
+
+
 }
 
 function processAllFieldsOfTheForm(req, res) {
@@ -61,4 +79,4 @@ function processAllFieldsOfTheForm(req, res) {
 
 
 server.listen(8081);
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");
